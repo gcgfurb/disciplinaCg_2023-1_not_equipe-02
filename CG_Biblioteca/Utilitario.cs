5ddd2103f21aa8de801eab8a16ea3c122b074aab
dalton-reis <dalton.reis@gmail.com>
2023-05-19 16:43:07 -0300

    .

--- CG_Biblioteca/Utilitario.cs
+++ CG_Biblioteca/Utilitario.cs
@@ -4,14 +4,36 @@ namespace gcgcg
 {
   public abstract class Utilitario
   {
-    public static char charProximo(char atual) {
+    public static char charProximo(char atual)
+    {
       return Convert.ToChar(atual + 1);
     }
+
+    public static char TeclaUpperConsole(String msg, ref bool Control, ref bool Shift)
+    {
+      Console.WriteLine(msg);
+      ConsoleKeyInfo input = Console.ReadKey(true);
+
+      if ((input.Modifiers & ConsoleModifiers.Control) == ConsoleModifiers.Control)
+      {
+        Control = true;
+      }
+      else
+      {
+        if ((input.Modifiers & ConsoleModifiers.Shift) == ConsoleModifiers.Shift)
+        {
+          Shift = true;
+        }
+      }
+      return Char.ToUpper(input.Key.ToString()[0]);
+    }
+
     public static void AjudaTeclado()
     {
       Console.WriteLine(" --- Ajuda / Teclas: ");
-      Console.WriteLine(" [  H     ] mostra está ajuda. ");
-      Console.WriteLine(" [Escape  ] sair. ");
+      Console.WriteLine(" [H] mostra está ajuda. ");
+      Console.WriteLine(" [Escape] sair. ");
+      Console.WriteLine(" [Barra de Espaço] imprimir Grafo de Cena. "); //TODO: quais teclas irei usar.
     }
 
   }
